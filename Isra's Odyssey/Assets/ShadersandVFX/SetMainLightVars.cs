434a1fdc4acdfee7ac5c014a5e70911f9f004b8b
Andrew Tiller <andrew.tiller@mymail.champlain.edu>
2021-09-10 14:44:27 -0400

    Toon Shader working

--- Isra's Odyssey/Assets/ShadersandVFX/SetMainLightVars.cs
+++ Isra's Odyssey/Assets/ShadersandVFX/SetMainLightVars.cs
@@ -1,21 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.HighDefinition;
 
+[ExecuteInEditMode]
 public class SetMainLightVars : MonoBehaviour
 {
     [SerializeField] Light mainLight;
-    [SerializeField] HDAdditionalLightData mainLightData;
-
-    void Start()
+    private void Update()
     {
-        //mainLightData.range
-        Shader.SetGlobalVector("_MainLightDirection", mainLight.transform.forward * -1);
-        Debug.Log("Set Direction to: " + mainLight.transform.forward);
-        Shader.SetGlobalFloat("_MainLightAttenuation", mainLight.range * 10);
-        Debug.Log("Set Attenuation to: " + mainLight.range);
-        Shader.SetGlobalColor("_MainLightColor", mainLight.color);
-        Debug.Log("Set Color to: " + mainLight.color);
+        Shader.SetGlobalVector("_ToonLightDirection", -transform.forward);
+        Shader.SetGlobalColor("_ToonLightColor", mainLight.color);
+        Shader.SetGlobalFloat("_ToonLightIntensity", mainLight.intensity);
     }
 }
